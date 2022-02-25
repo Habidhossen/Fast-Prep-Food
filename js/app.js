373310b934d09b2034201d610c430cbc77ab104c
Habidhossen <habidhossen2@gmail.com>
2022-02-25 19:24:41 +0600

    api conneced

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-const searchButoon = () => {
-  console.log("Clicks");
+const searchButton = () => {
+  const input = document.getElementById("input-value");
+  const inputValue = input.value;
+  if (inputValue == "") {
+    console.log("Type any food name");
+  } else {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+      .then((response) => response.json())
+      .then((data) => searchFood(data.meals));
+  }
+};
+const searchFood = (foodList) => {
+  console.log(foodList);
 };
